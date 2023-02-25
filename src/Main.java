105207e820f959741758769fc4baf4cd74584ba8
parulekarakshaycourses <119445348+parulekarakshaycourses@users.noreply.github.com>
2023-02-24 20:09:04 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         // This is 1st comment
+        // This is 2nd comment
     }
 }
